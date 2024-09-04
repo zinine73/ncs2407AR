8c97498a86df3be00cede3b4cb454deefda94c0d
zinine73 <zinine@naver.com>
2024-09-04 14:44:04 +0900

    LoadScene

---
+++ Assets/Scripts/BackToMain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMain : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene()
+            .buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }  
+    }
+}
--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// 씬전환을 위해서는 이게 필요
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneChange : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI progressText;
+
     public void OnChangeScene(string value)
     {
-        SceneManager.LoadScene(value);
+        // 간단한 씬 전환에는 LoadScene을 사용
+        //SceneManager.LoadScene(value);
+
+        // 시간이 걸리는 씬 전환에는 LoadSceneAsync를 사용
+        StartCoroutine(LoadScene(value));
+    }
+
+    IEnumerator LoadScene(string value)
+    {
+        AsyncOperation ao 
+        = SceneManager.LoadSceneAsync(value);
+        while (!ao.isDone)
+        {
+            progressText.text 
+            = $"Loading : {ao.progress * 100}%";
+            yield return null;
+        }
     }
 }
